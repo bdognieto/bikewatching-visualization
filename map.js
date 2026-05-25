c304deef956fb7ceb460de92a7205a96704dc286
bdognieto <bnieto@ucsd.edu>
2026-05-25 16:38:48 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,14 +1,19 @@
+// Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+// Check that Mapbox GL JS is loaded
+console.log('Mapbox GL JS Loaded:', mapboxgl);
 
-mapboxgl.accessToken = 'YOUR_TOKEN';
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1IjoiYmRvZ25pZXRvIiwiYSI6ImNtcGhjaWxxejB5eWYyeHB0ejI2Z2k0M3MifQ.guex31voVxI4t5rBI4L3Zw';
 
+// Initialize the map
 const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v12',
-  center: [-71.09415, 42.36027],
-  zoom: 12,
-  minZoom: 5,
-  maxZoom: 18,
+  container: 'map', // ID of the div where the map will render
+  style: 'mapbox://styles/mapbox/streets-v12', // Map style
+  center: [-71.09415, 42.36027], // [longitude, latitude]
+  zoom: 12, // Initial zoom level
+  minZoom: 5, // Minimum allowed zoom
+  maxZoom: 18, // Maximum allowed zoom
 });
 
 map.on('load', async () => {
@@ -23,9 +28,9 @@ map.on('load', async () => {
     type: 'line',
     source: 'boston_route',
     paint: {
-      'line-color': 'green',
-      'line-width': 3,
-      'line-opacity': 0.4,
+      'line-color': '#FF2D95',
+      'line-width': 4,
+      'line-opacity': 0.7,
     },
   });
 
