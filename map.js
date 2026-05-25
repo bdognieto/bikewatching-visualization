6d9f3c794efa6f0d33d692cc6c0d13fce15f724d
bdognieto <bnieto@ucsd.edu>
2026-05-25 16:17:04 -0700

    add bike routes

--- map.js
+++ map.js
@@ -1,17 +1,32 @@
-// Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
-// Check that Mapbox GL JS is loaded
-console.log('Mapbox GL JS Loaded:', mapboxgl);
 
-// Set your Mapbox access token here
-mapboxgl.accessToken = 'pk.eyJ1IjoiYmRvZ25pZXRvIiwiYSI6ImNtcGhjaWxxejB5eWYyeHB0ejI2Z2k0M3MifQ.guex31voVxI4t5rBI4L3Zw';
+mapboxgl.accessToken = 'YOUR_TOKEN';
 
-// Initialize the map
 const map = new mapboxgl.Map({
-  container: 'map', // ID of the div where the map will render
-  style: 'mapbox://styles/mapbox/streets-v12', // Map style
-  center: [-71.09415, 42.36027], // [longitude, latitude]
-  zoom: 12, // Initial zoom level
-  minZoom: 5, // Minimum allowed zoom
-  maxZoom: 18, // Maximum allowed zoom
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-71.09415, 42.36027],
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18,
+});
+
+map.on('load', async () => {
+
+  map.addSource('boston_route', {
+    type: 'geojson',
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+  });
+
+  map.addLayer({
+    id: 'bike-lanes',
+    type: 'line',
+    source: 'boston_route',
+    paint: {
+      'line-color': 'green',
+      'line-width': 3,
+      'line-opacity': 0.4,
+    },
+  });
+
 });
