3b04e27114992e61c12385467b2d2b606d4b0e23
bdognieto <bnieto@ucsd.edu>
2026-05-25 17:33:51 -0700

    update station visualizations

--- map.js
+++ map.js
@@ -68,6 +68,38 @@ map.on('load', async () => {
     console.log('Loaded JSON Data:', jsonData);
 
     let stations = jsonData.data.stations;
+
+    const trips = await d3.csv(
+      'https://dsc106.com/labs/lab07/data/bluebikes-traffic-2024-03.csv'
+    );
+
+    const departures = d3.rollup(
+      trips,
+      (v) => v.length,
+      (d) => d.start_station_id
+    );
+
+    const arrivals = d3.rollup(
+      trips,
+      (v) => v.length,
+      (d) => d.end_station_id
+    );
+
+    stations = stations.map((station) => {
+      let id = station.short_name;
+
+      station.arrivals = arrivals.get(id) ?? 0;
+      station.departures = departures.get(id) ?? 0;
+      station.totalTraffic = station.arrivals + station.departures;
+
+      return station;
+    });
+
+    const radiusScale = d3
+      .scaleSqrt()
+      .domain([0, d3.max(stations, (d) => d.totalTraffic)])
+      .range([0, 25]);
+
     const svg = d3.select('#map').select('svg');
 
     const circles = svg
@@ -75,7 +107,7 @@ map.on('load', async () => {
       .data(stations)
       .enter()
       .append('circle')
-      .attr('r', 5)
+      .attr('r', (d) => radiusScale(d.totalTraffic))
       .attr('fill', 'steelblue')
       .attr('stroke', 'white')
       .attr('stroke-width', 1)
